2b2699a440b819dc75b616f4a8ec91ceb6c0f657
ando <k-anod@tees-con.co.jp>
2020-04-16 16:19:28 +0900

    add:マテリアルデザインのnuget追加

---
+++ AmadeusController/AmadeusController/Main.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace AmadeusController
+{
+    public class Main
+    {
+        private static Process amadeus = new Process();
+
+        public static void Show()
+        {
+            amadeus.StartInfo.FileName = @"S:\Edit\AviUtl 2019\AviUtl\aviutl.exe";
+            amadeus.Start();
+        }
+
+        /// <summary>
+        /// amadeusが終了しているか
+        /// </summary>
+        /// <param name="param"></param>
+        public static void CheckExit(ref bool param)
+        {
+            param = amadeus.HasExited;
+        }
+    }
+}
