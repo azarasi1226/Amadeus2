52900538a99df381f52c457dd1b12a5716447c2d
ando <k-anod@tees-con.co.jp>
2020-04-16 11:17:18 +0900

    initial commit

---
+++ AmadeusController/AmadeusController/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmadeusController
+{
+    public static class Main
+    {
+        public static void Show()
+        {
+
+        }
+    }
+}
